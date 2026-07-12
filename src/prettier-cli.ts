16e00f641020e1441c60c8bd7b1d3ab31852a75f
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-12 03:27:01 +0200

    Remove superseded root CLI entrypoints

--- src/prettier-cli.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import { runStandaloneDevtoolsCommand } from './internal/runStandaloneDevtoolsCommand.js';
-
-const result = await runStandaloneDevtoolsCommand('format', process.argv.slice(2));
-process.exit(result.exitCode);
