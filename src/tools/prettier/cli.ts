c106b17ee98f9fafde86d647fb5cbcd938f8487a
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-12 03:46:53 +0200

    Move Prettier CLI under src tools

---
+++ src/tools/prettier/cli.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { runStandaloneDevtoolsCommand } from '../../internal/runStandaloneDevtoolsCommand.js';
+
+const result = await runStandaloneDevtoolsCommand('format', process.argv.slice(2));
+process.exit(result.exitCode);
