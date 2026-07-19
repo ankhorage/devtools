86ce0194504e2c8f67a0708e485628d1d9990b32
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-19 03:42:31 +0200

    Add ESLint plugin type declarations

---
+++ src/types/eslint-plugins.d.ts
@@ -0,0 +1,13 @@
+declare module 'eslint-plugin-security' {
+  import type { Linter } from 'eslint';
+
+  const plugin: Linter.Plugin;
+  export default plugin;
+}
+
+declare module 'eslint-plugin-react-native' {
+  import type { Linter } from 'eslint';
+
+  const plugin: Linter.Plugin;
+  export default plugin;
+}
